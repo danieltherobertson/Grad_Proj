e3936e43851c59fc1f2a6dd2d9a38c90558e9f90
Daniel Robertson <danieltherobertson@gmail.com>
2016-05-13 17:32:16 +0100

    Creates DispatchMenuView class and Nib, adds design for Dispatch Menu View

---
+++ TheOperator/DispatchMenuView.swift
@@ -0,0 +1,13 @@
+//
+//  DispatchMenuView.swift
+//  TheOperator
+//
+//  Created by Daniel Robertson on 13/05/2016.
+//  Copyright © 2016 Daniel Robertson. All rights reserved.
+//
+
+import UIKit
+
+class DispatchMenuView: UIView {
+    
+}
