9bf92d52e7968ccba91af8bab78266efa4462edf
Daniel Robertson <danieltherobertson@gmail.com>
2016-05-22 01:35:34 +0100

    Adds eventView and related files and assets

---
+++ TheOperator/EventView.swift
@@ -0,0 +1,21 @@
+//
+//  EventView.swift
+//  TheOperator
+//
+//  Created by Daniel Robertson on 22/05/2016.
+//  Copyright © 2016 Daniel Robertson. All rights reserved.
+//
+
+import UIKit
+
+class EventView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
