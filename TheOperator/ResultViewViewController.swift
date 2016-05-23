90f42e941b37b08e1247fa59513c18f6e08da258
Daniel Robertson <danieltherobertson@gmail.com>
2016-05-23 23:50:31 +0100

    Adds animation and loading gif to ResultsViewVC

--- TheOperator/ResultViewViewController.swift
+++ TheOperator/ResultViewViewController.swift
@@ -12,18 +12,35 @@ class ResultViewViewController: UIViewController {
     
 
 
+    @IBOutlet weak var sirensGif: UIImageView!
     @IBOutlet weak var containerView: UIView!
+    
+    let gif = UIImage.gifWithName("operator")
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let triggerTime = Int64(4 * (NSEC_PER_SEC))
-//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, triggerTime), dispatch_get_main_queue(), {
-//            let newsPaper = EventView.instanceFromNib()
-//            newsPaper.frame = CGRect(x: 0, y: 0, width: self.containerView.bounds.width, height: self.containerView.bounds.height)
-//            newsPaper.showInView(self.containerView, animated: true)
-//        })
-
+        sirensGif.image = gif
+        sirensGif.alpha = 0
+        sirensGif.transform = CGAffineTransformMakeScale(1.3, 1.3)
+        containerView.alpha = 0
+        self.containerView.transform = CGAffineTransformMakeScale(1.3, 1.3)
+        let triggerTime = Int64(2 * (NSEC_PER_SEC))
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, triggerTime), dispatch_get_main_queue(), {
+            UIView.animateWithDuration(0.50, animations: { 
+                self.sirensGif.alpha = 1.0
+                self.sirensGif.transform = CGAffineTransformMakeScale(1.0, 1.0)
+                }, completion: { (true) in
+                    let triggerTime = Int64(3 * (NSEC_PER_SEC))
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, triggerTime), dispatch_get_main_queue(), {
+                        self.sirensGif.alpha = 0
+                        UIView.animateWithDuration(0.50, animations: {
+                            self.containerView.alpha = 1.0
+                            self.containerView.transform = CGAffineTransformMakeScale(1.0, 1.0)
+                        });
+                    })
+            })
+        })
     }
 
     
